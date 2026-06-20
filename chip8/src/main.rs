77d5917251b9b56c9881b6f079450e79cb882a0a
grsylvia <grsgarrett@gmail.com>
2026-06-19 23:25:47 -0400

    Add initial Chip8 struct

--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Chip8 {
+    memory: [u8; 4096], // 4096 bytes of RAM, each cell is a byte
+    v: [u8; 16], // general registers, V0-VF
+    i: u16, // index register, holds addresses
+    pc: u16, // program counter, address of next instruction
+    stack: [u16; 16], // stack, saves return addresses
+    sp: u8, // stack pointer
+    delay_timer: u8,
+    sound_timer: u8,
+    display: [bool; 64 * 32], // 64 * 32 screen, each pixel on or off
+    keypad: [bool; 16] // 16 keys, pressed or not
+}
